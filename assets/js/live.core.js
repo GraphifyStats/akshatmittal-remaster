775e3f76ed019582f34b5cdba787ed9714bd0f20
Not TCA <realtcatech@gmail.com>
2022-12-23 13:43:22 +0800

    update api to lc.xyz

--- assets/js/live.core.js
+++ assets/js/live.core.js
@@ -1,11 +1,17 @@
 YT.live = {
     channelID: "",
     update: function () {
+        $.getJSON("https://livecounts.xyz/api/youtube-live-subscriber-count/live/" + this.channelID, function (e) {
+            if(e) {
+                YT.updateManager.updateSubscribers(e.counts[0]);
+                YT.updateManager.updateVideos(e.counts[2]);
+            } else {
+                YT.query.newSearch(YT.live.channelID);
+            }
+        })
         $.getJSON("https://mixerno.space/api/youtube-channel-counter/user/" + this.channelID, function (e) {
             if (e) {
-                YT.updateManager.updateSubscribers(e.counts[2].count);
                 YT.updateManager.updateViews(e.counts[3].count);
-                YT.updateManager.updateVideos(e.counts[5].count);
             } else {
                 YT.query.newSearch(YT.live.channelID);
             }
