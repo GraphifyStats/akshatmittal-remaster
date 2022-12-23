5d2dc57982c3322d026f30a329fc998e58d73dc6
Not TCA <realtcatech@gmail.com>
2022-12-23 14:09:16 +0800

    update compare count

--- assets/js/live.vs.js
+++ assets/js/live.vs.js
@@ -1,28 +1,36 @@
 YT.live = {
-    vs1: "",
-    vs2: "",
-    update: function () {
-        $.getJSON("https://mixerno.space/api/youtube-channel-counter/user/" + YT.live.vs1, function (f) {
-            $.getJSON("https://mixerno.space/api/youtube-channel-counter/user/" + YT.live.vs2, function (g) {
-                YT.updateManager.updateSubscribers(f.counts[2].count, g.counts[2].count);
-            });
-        });
-    },
-    timer: null,
-    setVS: function (e, f) {
-        this.vs1 = e;
-        this.vs2 = f;
-        this.start();
-    },
-    start: function () {
-        this.stop();
-        YT.query.begin();
-        this.timer = setInterval(function (e) {
-            YT.live.update();
-        }, 10000);
-        YT.live.update();
-    },
-    stop: function () {
-        clearInterval(this.timer);
-    }
+  vs1: "",
+  vs2: "",
+  update: function () {
+    $.getJSON(
+      "https://livecounts.xyz/api/youtube-live-subscriber-count/live/" +
+        YT.live.vs1,
+      function (f) {
+        $.getJSON(
+          "https://livecounts.xyz/api/youtube-live-subscriber-count/live/" +
+            YT.live.vs2,
+          function (g) {
+            YT.updateManager.updateSubscribers(f.counts[0], g.counts[0]);
+          }
+        );
+      }
+    );
+  },
+  timer: null,
+  setVS: function (e, f) {
+    this.vs1 = e;
+    this.vs2 = f;
+    this.start();
+  },
+  start: function () {
+    this.stop();
+    YT.query.begin();
+    this.timer = setInterval(function (e) {
+      YT.live.update();
+    }, 10000);
+    YT.live.update();
+  },
+  stop: function () {
+    clearInterval(this.timer);
+  },
 };
